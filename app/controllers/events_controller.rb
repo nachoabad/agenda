ad01eff6219eae65a23895b172dd4f583ea4f165
Nacho Abad <nachoabad@nebulab.com>
2023-08-29 23:53:18 +0200

    orders events by date then by slot rule time

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,7 @@ class EventsController < ApplicationController
       current_user.events
     end
 
-    @events = events.booked.where(date: Date.today..).order(:date)
+    @events = events.includes(:slot_rule).booked.where(date: Date.today..).order(:date, "slot_rules.time")
   end
 
   def show
