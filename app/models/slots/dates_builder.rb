ba954d16b9fe95c477f54f279c0fd9af7af041ae
Nacho Abad <nachoabad@nebulab.com>
2023-08-30 18:42:54 +0200

    adds Slots::DatesBuilder

---
+++ app/models/slots/dates_builder.rb
@@ -0,0 +1,41 @@
+module Slots
+  class DatesBuilder
+    DATES_RANGE = 6.days
+    
+    def initialize(date:, service:, user:)
+      @date = date
+      @service = service
+      @user = user
+    end
+
+    def build
+      dates = @date..(@date + DATES_RANGE)
+
+      slot_rules = @service.slot_rules.active.sort_by(&:time).group_by &:wday    
+      events     = @service.events.where(date: dates)
+
+      slots = {}
+      dates.each do |date|
+        if slot_rules[date.wday]
+          slot_rules[date.wday].each do|slot_rule|
+            event = events.find_by(date:, slot_rule:)
+            unless event && !@user.owns?(event.service)
+              slot_date_time = slot_rule.date_time(date).in_time_zone(@user.time_zone)
+              slots[slot_date_time.to_date] ||= []
+              slots[slot_date_time.to_date] << Slot.new(
+                slot_date_time,
+                slot_rule,
+                event,
+                date
+              )
+            end
+          end.compact
+        else
+          next
+        end
+      end
+
+      slots.compact_blank!
+    end
+  end
+end
