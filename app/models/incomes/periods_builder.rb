f0dc86fbe290f1d42d0f8898e5f43afc90374ccf
Nacho Abad <nachoabad@nebulab.com>
2023-10-12 23:14:45 +0200

    orders incomes by date

--- app/models/incomes/periods_builder.rb
+++ app/models/incomes/periods_builder.rb
@@ -10,7 +10,7 @@ module Incomes
 
     def build
       dates = (@date - DATES_RANGE)..@date
-      periods = @service.payments.not_pending.where(date: dates).group_by do |payment|
+      periods = @service.payments.not_pending.where(date: dates).order(date: :desc).group_by do |payment|
         payment.date.beginning_of_month
       end.transform_values do |period_payments|
         period_payments.group_by(&:currency).transform_values do |currency_payments|
