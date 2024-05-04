1fb2146725a8686737f0fb22d1cf3cbff9adb253
Nacho Abad <nacho@upmailsolutions.com>
2024-05-04 20:58:31 +0200

    simplify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,10 @@
 admin = User.create_with(password: "adminadmin", password_confirmation: "adminadmin", name: "Graciela Olmos", time_zone: "Madrid").find_or_create_by! email: "admin@mail.com"
 service = admin.services.first_or_create! name: "Consultas", time_zone: "Madrid"
 
-admin2 = User.create_with(password: "adminadmin", password_confirmation: "adminadmin", name: "Pablo Raydan", time_zone: "Madrid").find_or_create_by! email: "admin2@mail.com"
-service2 = admin2.services.first_or_create! name: "Consultas Recurrents", time_zone: "Madrid", accepts_event_rules: true,
-settings: {service_types: ["Passport", "Notary", "Notary w/ witness (Mon,Wed,Fri)"]}
+user = User.create_with(password: "useruser", password_confirmation: "useruser", name: "Nacho Abad", time_zone: "Eastern Time (US & Canada)").find_or_create_by! email: "user@mail.com"
 
-user = User.create_with(password: "useruser", password_confirmation: "useruser", name: "María Miami", time_zone: "Eastern Time (US & Canada)").find_or_create_by! email: "user@mail.com"
-
-service2.slot_rules.destroy_all
-service2.slot_rules.create! [
+service.slot_rules.destroy_all
+service.slot_rules.create! [
   {time: "10:00", wday: 1},
   {time: "11:30", wday: 1},
   {time: "13:00", wday: 1},
